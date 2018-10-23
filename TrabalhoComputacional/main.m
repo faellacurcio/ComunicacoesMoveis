dbe489e0e8349a647c58c000f4884b4598eb1d0e
Rafael Acurcio <rafael.acurcio@gmail.com>
2018-10-23 11:17:03 -0300

    Agora o mesmo par de usuarios sofre os efeitos dos 4 cenários propostos pelo trabalho. As CDFs são normalizadas para serem plotadas no mesmo gráfico. E acho que só.

--- TrabalhoComputacional/main.m
+++ TrabalhoComputacional/main.m
@@ -9,6 +9,10 @@ ERB = [0 0 2000];
 
 global TM_table
 
+global PicoCell
+
+PicoCell = [1700 0 300];
+
 global max_users
 
 max_users = 2;
@@ -41,6 +45,7 @@ shouldPlotMap = 0;
 
 %% -- Plot elements --
 if(shouldPlotMap)
+    setUsers()
     scatter(ERB(1),ERB(2),'*')
     hold on
     scatter(PicoCell(1),PicoCell(2),'*')
@@ -55,12 +60,18 @@ end
 
 %% -- MAIN  --
 result1matrix = [];
+result2matrix = [];
+result3matrix = [];
+result4matrix = [];
+    
 aux = [];
-max_iterations = 100;
+max_iterations = 500;
 
-%% Primeira questão item 1
 
+% Loop
 for loop = 1:max_iterations
+ 
+    %% Primeira questão item 1
     
     %Create new users randomly spread through the area
     setUsers()
@@ -74,18 +85,13 @@ for loop = 1:max_iterations
     aux(2) = capacity(1, SINR);
     
     result1matrix = [result1matrix; aux];
-end
 
-%% Primeira questão item 2
+    %% Primeira questão item 2
 
-result2matrix = [];
-aux = [];
-timepercentage = 0.5;
+    aux = [];
+    timepercentage = 0.5;
 
-for loop = 1:max_iterations
-    
-    %Create new users randomly spread through the area
-    setUsers()
+
     
     %SINR for the MACRO CELL
     SINR = powerMacro / (pathLoss(hypot(TM_table(1,1),TM_table(1,2))) + powerNoise);
@@ -96,19 +102,13 @@ for loop = 1:max_iterations
     aux(2) = capacity(1-timepercentage, SINR);
     
     result2matrix = [result2matrix; aux];
-end
 
 %% Primeira questão item 3
 
-result3matrix = [];
-aux = [];
-angle = 10;
 
-for loop = 1:max_iterations
-    
-    %Create new users randomly spread through the area
-    setUsers()
-    
+    aux = [];
+    angle = 10;
+
     %SINR for the MACRO CELL
     SINR = (powerMacro + min(20,360/angle)) / (pathLoss(hypot(TM_table(1,1),TM_table(1,2))) + powerNoise);
     aux(1) = capacity(1, SINR);
@@ -118,18 +118,11 @@ for loop = 1:max_iterations
     aux(2) = capacity(1, SINR);
     
     result3matrix = [result3matrix; aux];
-end
 
 %% Primeira questão item 4
 
-result4matrix = [];
-aux = [];
-timepercentage = 0.5;
-
-for loop = 1:max_iterations
-    
-    %Create new users randomly spread through the area
-    setUsers()
+    aux = [];
+    timepercentage = 0.5;
     
     %SINR for the MACRO CELL
     SINR = (powerMacro + min(20,360/angle)) / (pathLoss(hypot(TM_table(1,1),TM_table(1,2))) + powerNoise);
@@ -150,17 +143,21 @@ for loop = 1:max_iterations
     result4matrix = [result4matrix; aux];
     
 end
+
+cdfCompare(result1matrix)
+cdfCompare(result2matrix)
+cdfCompare(result3matrix)
+cdfCompare(result4matrix)
+
 %% -- Function declarations --
 
 % -- Set Users  --
 function setUsers()
     
     global max_users
-    
+    global PicoCell
     radius_usr1 = 2000;
     radius_usr2 = 300;
-    
-    PicoCell = [1700 0 300];
     
     % randRadius = [radius*sqrt(rand(max_users,1))];
     randRadius = [radius_usr1*sqrt(rand(1,1)); radius_usr2*sqrt(rand(1,1))];
@@ -172,20 +169,36 @@ function setUsers()
     %               |----------------------X,Y---------------------|-----------Picocell---------|
     global TM_table
     TM_table    =   [randRadius.*cos(angle) randRadius.*sin(angle)]+[0 0;PicoCell(1) PicoCell(2)];
+    
 end
 
-function C = capacity(uovetT, SINR) %(bits/s)
+function C = capacity(uovertT, SINR) %(bits/s)
+    
     global B
     global alpha
     global T
     global beta
-    C = min((uovetT/T)*alpha*B*log2(1+SINR/beta),10^7);
+    C = min((uovertT/T)*alpha*B*log2(1+SINR/beta),10^7);
+        
 end
 
 function pathloss = pathLoss(d)
+
     global n
     global d0
     pathloss = 38.4 + 10*n*log10(d/d0);
+
 end
 
+function cdfCompare(datamatrix)
+    figure
+    cdfplot(normalizeData(datamatrix(:,1)))
+    hold on
+    cdfplot(normalizeData(datamatrix(:,2)))
+    legend('User 1 (macro)','User 2 (pico)','Location','best')
+    hold off
+end
 
+function result = normalizeData(data)
+    result = (data - min(data)) / ( max(data) - min(data) );
+end
